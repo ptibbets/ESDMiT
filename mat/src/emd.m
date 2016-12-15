0b0fb5970cf001449ffd117ad9c65e58d924da12
ptibbets <misio4x4@gmail.com>
2016-12-15 20:25:36 +0100

    Added all files

--- mat/src/emd.m
+++ mat/src/emd.m
@@ -12,28 +12,50 @@ function imf = emd(x)
        while SD > 0.3
           d = diff(h);
           maxmin = [];
-          for i = 1:N - 2
-             if d(i) == 0
-                maxmin = [maxmin, i];
-             elseif sign(d(i)) ~= sign(d(i+1))
-                maxmin = [maxmin, i+1];
+          maxes = [];
+          mins = [];
+          for i = 2:N - 2
+             if d(i) == 0 && d(i - 1) > 0 && d(i + 1) < 0
+                maxes = [maxes, i];
+             elseif d(i) == 0 && d(i - 1) < 0 && d(i + 1) > 0
+                 mins = [mins, i];
+             elseif d(i) > 0 && d(i + 1) < 0
+                 maxes = [maxes, i + 1];
+             elseif d(i) < 0 && d(i + 1) > 0
+                 mins = [mins, i + 1];
              end
+%              if d(i) == 0 && ((d(i - 1) > 0 && d(i + 1) < 0) || ...
+%                      (d(i - 1) < 0 && d(i + 1) > 0))
+%                 maxmin = [maxmin, i];
+%              elseif sign(d(i)) ~= sign(d(i + 1))
+%                 maxmin = [maxmin, i + 1];
+%              end
           end
 
-          if size(maxmin, 2) < 2
+          if size(maxes, 2) + size(mins, 2) < 2
              break
           end
 
-          if maxmin(1) > maxmin(2)
-             maxes = maxmin(1:2:length(maxmin));
-             mins  = maxmin(2:2:length(maxmin));
-          else
-             maxes = maxmin(2:2:length(maxmin));
-             mins  = maxmin(1:2:length(maxmin));
+%           if maxmin(1) > maxmin(2)
+%              maxes = maxmin(1:2:length(maxmin));
+%              mins  = maxmin(2:2:length(maxmin));
+%           else
+%              maxes = maxmin(2:2:length(maxmin));
+%              mins  = maxmin(1:2:length(maxmin));
+%           end
+          
+          if(maxes(1) ~= 1)
+              maxes = [1 maxes];
           end
-
-          maxes = [1 maxes N];
-          mins  = [1 mins  N];
+          if(maxes(length(maxes)) ~= N)
+              maxes = [maxes N];
+          end
+          if(mins(1) ~= 1)
+              mins = [1 mins];
+          end
+          if(mins(length(mins)) ~= N)
+              mins = [mins N];
+          end
 
           maxenv = spline(maxes, h(maxes), 1:N);
           minenv = spline(mins, h(mins), 1:N);
@@ -49,7 +71,7 @@ function imf = emd(x)
 
        imf = [imf; h];
 
-       if size(maxmin, 2) < 2
+       if size(maxes, 2) + size(mins, 2) < 2
           break
        end
 
