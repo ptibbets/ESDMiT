c38ad9d5707a6f6e138f614aeabc366dc4d3328b
ptibbets <misio4x4@gmail.com>
2017-01-09 22:37:45 +0100

    Created diff helper

---
+++ cpp/helpers/diff.cpp
@@ -0,0 +1,13 @@
+#include "diff.h"
+
+Eigen::VectorXd diff(Eigen::VectorXd const & vVector)
+{
+    Eigen::VectorXd aVec(vVector.size() - 1);
+
+    for(auto aIndex = 0; aIndex < aVec.size(); aIndex++)
+    {
+        aVec[aIndex] = vVector[aIndex + 1] - vVector[aIndex];
+    }
+
+    return aVec;
+}
