71da2c5f2a790bdf6594874fdae9c13bef1f1229
ptibbets <misio4x4@gmail.com>
2017-01-10 23:58:32 +0100

    Deleted Vector helper

--- cpp/emd.cpp
+++ cpp/emd.cpp
@@ -2,34 +2,51 @@
 #include "helpers/diff.h"
 #include "helpers/Vector.h"
 
-void findMinMax(Eigen::VectorXd &vDiff, helpers::Vector<Eigen::VectorXi> &vMins, helpers::Vector<Eigen::VectorXi> &vMaxes)
+void findMinMax(Eigen::VectorXd &vDiff, Eigen::VectorXi &vMins, Eigen::VectorXi &vMaxes)
 {
-    for(auto aIndex = 1; aIndex < vDiff.size() - 3; aIndex++)
+    std::size_t aMinLength = 0, aMaxLength = 0;
+    vMaxes[aMaxLength++] = 0;
+    vMins[aMinLength++] = 0;
+    for(auto aIndex = 1; aIndex < vDiff.size() - 2; aIndex++)
     {
         if((vDiff[aIndex] == 0) && (vDiff[aIndex - 1] > 0) && (vDiff[aIndex + 1] < 0))
         {
-            vMaxes.pushBack(aIndex);
+            vMaxes[aMaxLength++] = aIndex;
         }
         else if((vDiff[aIndex] == 0) && (vDiff[aIndex - 1] < 0) && (vDiff[aIndex + 1] > 0))
         {
-            vMins.pushBack(aIndex);
+            vMins[aMinLength++] = aIndex;
         }
         else if((vDiff[aIndex] > 0) && (vDiff[aIndex + 1] < 0))
         {
-            vMaxes.pushBack(aIndex + 1);
+            vMaxes[aMaxLength++] = aIndex + 1;
         }
         else if((vDiff[aIndex] < 0) && (vDiff[aIndex + 1] > 0))
         {
-            vMins.pushBack(aIndex + 1);
+            vMins[aMinLength++] = aIndex + 1;
         }
     }
+
+    if(aMaxLength + aMinLength < 2)
+    {
+        throw std::exception();
+    }
+
+    if(vMaxes[aMaxLength] != vDiff.size() - 1)
+    {
+        vMaxes[aMaxLength++] = vDiff.size();
+    }
+    if(vMins[aMinLength] != vDiff.size() - 1)
+    {
+        vMins[aMinLength++] = vDiff.size();
+    }
+    vMaxes = vMaxes.block(0, 0, aMaxLength, 1);
+    vMins = vMins.block(0, 0, aMinLength, 1);
 }
 
 std::vector<Eigen::VectorXd> emd(Eigen::VectorXd const &vSamples)
 {
     std::vector<Eigen::VectorXd> aIMF;
-    Eigen::VectorXd aMaxes(vSamples.size());
-    Eigen::VectorXd aMins(vSamples.size());
     auto aSamples = vSamples;
 
     while(true)
@@ -39,7 +56,17 @@ std::vector<Eigen::VectorXd> emd(Eigen::VectorXd const &vSamples)
 
         while(aSD > 0.3)
         {
-//            auto aDiff = helpers::diff(aH);
+            auto aDiff = helpers::diff(aH);
+            Eigen::VectorXi aMaxes(vSamples.size());
+            Eigen::VectorXi aMins(vSamples.size());
+            try
+            {
+                findMinMax(aDiff, aMins, aMaxes);
+            }
+            catch (std::exception &e)
+            {
+                break;
+            }
         }
     }
 
