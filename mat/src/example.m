a0d5020f32c535062701e8b9fb69ef1d7c8d5adc
ptibbets <misio4x4@gmail.com>
2017-01-11 21:48:23 +0100

    Added comparision with Cpp result on plot

--- mat/src/example.m
+++ mat/src/example.m
@@ -6,21 +6,28 @@ load('../dat/100.mat')
 t = tm(1:250,:);
 s = signal(1:250,1);
 
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,s,t,emd_filter(s,1))
 grid on
 legend('Original signal', '1 IMF discarded')
 ylabel('Amplitude')
 
-subplot(3,1,2)
+subplot(4,1,2)
 plot(t,s,t,emd_filter(s,2))
 grid on
 legend('Original signal', '2 IMF discarded')
 ylabel('Amplitude')
 
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,s,t,emd_filter(s,3))
 grid on
 legend('Original signal', '3 IMF discarded')
 ylabel('Amplitude')
 xlabel('Time [s]')
+
+load('cpp.mat')
+subplot(4,1,4)
+plot(t, s, t, cpp)
+grid on
+legend('Original signal', '1 IMF discarded')
+ylabel('Amplitude')
