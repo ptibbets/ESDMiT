a60d5699d43a03ac83e352c678ad6ac08758f09e
ptibbets <misio4x4@gmail.com>
2017-01-12 22:28:45 +0100

    Added Release configuration and modified diff function

--- cpp/helpers/diff.cpp
+++ cpp/helpers/diff.cpp
@@ -4,14 +4,11 @@
 Eigen::VectorXd helpers::diff(Eigen::VectorXd const & vVector)
 {
     Eigen::VectorXd aVec(vVector.rows() - 1);
+    double aDiff;
 
     for(auto aIndex = 0; aIndex < aVec.rows(); aIndex++)
     {
-        auto aDiff = vVector[aIndex + 1] - vVector[aIndex];
-        if(std::isnan(aDiff))
-        {
-            throw std::exception();
-        }
+        aDiff = vVector[aIndex + 1] - vVector[aIndex];
         aVec[aIndex] = aDiff;
     }
 
